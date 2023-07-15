2aab0a1e048d74df4fcdfe754fad78c8f3869d91
Seyma Cengiz <cengizseyma2021@gmail.com>
2023-07-15 17:38:29 +0200

    http interceptor added

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -19,13 +19,8 @@ export class DataStorageService {
     }
 
     fetchData() {
-        return this.authService.user.pipe(take(1),
-        exhaustMap(user=>{
-            return this.http.get<Recipe[]>('firebase_api_url.json',
-            {
-                params: new HttpParams().set('auth',user.token)
-            });
-        }),
+        return this.http.get<Recipe[]>('firebase_api_url.json')
+        .pipe(
         map(recipes => {
             return recipes.map(recipe => {
                 return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
